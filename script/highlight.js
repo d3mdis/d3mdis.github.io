cb10d9e2312839a38f7462677846962c59c20486
d3mdis <d3mdis@gmail.com>
2023-04-23 19:46:31 +0200

    Initial commit

---
+++ script/highlight.js
@@ -0,0 +1,22 @@
+const sections = document.querySelectorAll('#about, #education, #skills');
+const navBarLi = document.querySelectorAll('nav ul li');
+const snap = document.querySelector('.snap');
+
+snap.addEventListener('scroll', ()=> {
+    let current = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if(snap.scrollTop+700 > sectionTop){
+            current = section.getAttribute('id');
+            
+        }
+    })
+    navBarLi.forEach( li => {
+        li.classList.remove('active');
+        if(li.classList.contains(current)){
+            li.classList.add('active');
+        }
+    })
+})
